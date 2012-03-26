a9f3a35abda8ece17fe62eedb5a262e2c782c673
Arion Hardison <arion@dietmanager.com>
2012-03-26 04:07:14 -0700

    pic menu start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PictureMenu::Application.config.secret_token = 'ba50b1602cbb737eeadfedcbc9f8a349e2cdeb73bb643c35c37b24dde0f0ca99d8cf2b7f5389e8471b66dbd2c0f492ac4077eb2ad7225be840947320815f0025'
